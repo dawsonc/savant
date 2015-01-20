a5e1ac73dda64bdaf2d2d0493c441313e02ce591
Charles Dawson <charles.dwsn@gmail.com>
2015-01-20 18:37:26 -0500

    Extract feedforward code into separate function. General refactoring.

--- soldier.py
+++ soldier.py
@@ -9,23 +9,6 @@ import numpy as np
 class NeuralNetwork(object):
 
     """A neural network with one hidden layer"""
-    @staticmethod
-    def random_initial_weights(incoming_connections, outgoing_connections):
-        """Return the random intial weights for a layer with the given number \
-of connections
-
-        Arguments:
-            incoming_connections -- the number of connections going into each
-                                     neuron in the layer
-            outgoing_connections -- the number of neurons in this layer
-        """
-        epsilon = np.sqrt(
-            6) / np.sqrt(incoming_connections + outgoing_connections)
-        # +1 accounts for bias node
-        weights = np.random.rand(
-            outgoing_connections, incoming_connections + 1)
-        weights = weights * (2 * epsilon) - epsilon
-        return weights
 
     def __init__(self, input_layer_size, hidden_layer_size, num_outputs, reg,
                  activation_function, activation_function_gradient,
@@ -61,16 +44,32 @@ neurons
         self.output_activation_func_gradient = output_activation_func_gradient
 
     def train(self, indicators, labels):
+        # Train
         options = {'maxiter': 100, 'disp': True}
         nn_params = np.concatenate((self.theta1.ravel(), self.theta2.ravel()))
         costf = lambda params: self.cost(indicators, labels, params)
         result = optimize.minimize(costf, nn_params, jac=True, method='TNC',
-            options=options)
+                                   options=options)
 
-        print("Result: %s\nMessage: %s" % (result.success, result.message))
+        # Extract & save thetas
+        self.theta1, self.theta2 = self._extract_thetas(result.x)
 
     def predict(self, indicators):
-        pass
+        """Quick & dirty feedfoward to produce predictions
+
+        Target for future refactoring (extract feedfoward into a function)
+
+        Returns (predictions, confidence), where:
+            predictions is a matrix of predictions for each example
+            confidence is a matrix of probabilities for each label
+        """
+        # Part Ia: Feedforward
+
+        _, _, _, _, hypotheses = self._feedforward(indicators,
+                                                   self.theta1, self.theta2)
+        predictions = np.rint(hypotheses)
+
+        return (predictions, hypotheses)
 
     def cost(self, indicators, labels, nn_params):
         """Computes the cost and gradient for the network using \
@@ -92,12 +91,55 @@ and theta2.ravel()
 theta1 and theta2
         """
         # First get theta1 and theta2 back into fighting shape
-        theta1 = nn_params[0:self.theta1.size]
-        theta1.shape = self.theta1.shape
-        theta2 = nn_params[self.theta1.size:]
-        theta2.shape = self.theta2.shape
+        theta1, theta2 = self._extract_thetas(nn_params)
 
         # Then define some useful values
+        num_examples = indicators.shape[0]
+
+        # Now start the music
+        # Part Ia: Feedforward
+        indicators, z_hidden, activation_hidden, z_output, hypotheses \
+            = self._feedforward(indicators, theta1, theta2)
+
+        # Part Ib: Cost Function
+
+        # Sum the cost
+        cost = np.sum((-1 * labels) * np.log(hypotheses) -
+                      (1 - labels) * np.log(1 - hypotheses))
+        cost *= (1 / num_examples)
+
+        # Add regularization
+        cost += self.reg / (2 * num_examples) * \
+            (np.sum(theta1[:, 1:] ** 2) + np.sum(theta2[:, 1:] ** 2))
+
+        # Part II: Backpropogation
+        # Vectorized :)
+        delta3 = (hypotheses - labels)
+        delta2 = np.dot(delta3, theta2)
+        delta2 = delta2[:, 1:] * self.activation_function_gradient(z_hidden)
+        D1 = np.dot(indicators.T, delta2).T
+        D2 = np.dot(activation_hidden.T, delta3).T
+
+        theta1_grad = np.zeros(theta1.shape)
+        theta2_grad = np.zeros(theta2.shape)
+        # Regularize (except for bias term)
+        theta1_grad[:, 0] = 1 / num_examples * D1[:, 0]
+        theta1_grad[:, 1:] = 1 / num_examples * D1[:, 1:] + \
+            self.reg / num_examples * theta1[:, 1:]
+        theta2_grad[:, 0] = 1 / num_examples * D2[:, 0]
+        theta2_grad[:, 1:] = 1 / num_examples * D2[:, 1:] + \
+            self.reg / num_examples * theta2[:, 1:]
+
+        # Unroll gradients
+        gradients = np.concatenate((theta1_grad.ravel(), theta2_grad.ravel()))
+        return (cost, gradients)
+
+    def _feedforward(self, indicators, theta1, theta2):
+        """Performs feedforward on the given data, returning a tuple of results
+
+        Returns (indicators, z_hidden, activation_hidden, z_output, hypotheses)
+        """
+        # Define some useful values
         num_examples = indicators.shape[0]
 
         # Now start the music
@@ -123,38 +165,34 @@ theta1 and theta2
         # . hypotheses is a num_examples x self.o_size array
         hypotheses = self.output_activation_func(z_output)
 
-        # Part Ib: Cost Function
+        return (indicators, z_hidden, activation_hidden, z_output, hypotheses)
 
-        # Sum the cost
-        cost = np.sum((-1 * labels) * np.log(hypotheses) -
-                      (1 - labels) * np.log(1 - hypotheses))
-        cost *= (1 / num_examples)
+    def _extract_thetas(self, nn_params):
+        """Returns thetas extracted from 1D param array"""
+        theta1 = nn_params[0:self.theta1.size]
+        theta1.shape = self.theta1.shape
+        theta2 = nn_params[self.theta1.size:]
+        theta2.shape = self.theta2.shape
 
-        # Add regularization
-        cost += self.reg / (2 * num_examples) * \
-            (np.sum(theta1[:, 1:] ** 2) + np.sum(theta2[:, 1:] ** 2))
+        return (theta1, theta2)
 
-        # Part II: Backpropogation
-        # Vectorized :)
-        delta3 = (hypotheses - labels)
-        delta2 = np.dot(delta3, theta2)
-        delta2 = delta2[:, 1:] * self.activation_function_gradient(z_hidden)
-        D1 = np.dot(indicators.T, delta2).T
-        D2 = np.dot(activation_hidden.T, delta3).T
+    @staticmethod
+    def random_initial_weights(incoming_connections, outgoing_connections):
+        """Return the random intial weights for a layer with the given number \
+of connections
 
-        theta1_grad = np.zeros(theta1.shape)
-        theta2_grad = np.zeros(theta2.shape)
-        # Regularize (except for bias term)
-        theta1_grad[:, 0] = 1/num_examples * D1[:, 0]
-        theta1_grad[:, 1:] = 1/num_examples * D1[:, 1:] + \
-            self.reg/num_examples * theta1[:, 1:]
-        theta2_grad[:, 0] = 1/num_examples * D2[:, 0]
-        theta2_grad[:, 1:] = 1/num_examples * D2[:, 1:] + \
-            self.reg/num_examples * theta2[:, 1:]
-
-        # Unroll gradients
-        gradients = np.concatenate((theta1_grad.ravel(), theta2_grad.ravel()))
-        return (cost, gradients)
+        Arguments:
+            incoming_connections -- the number of connections going into each
+                                     neuron in the layer
+            outgoing_connections -- the number of neurons in this layer
+        """
+        epsilon = np.sqrt(
+            6) / np.sqrt(incoming_connections + outgoing_connections)
+        # +1 accounts for bias node
+        weights = np.random.rand(
+            outgoing_connections, incoming_connections + 1)
+        weights = weights * (2 * epsilon) - epsilon
+        return weights
 
 
 def sigmoid(z):
--- train_check.py
+++ train_check.py
@@ -4,22 +4,35 @@ from scipy.io import loadmat
 import numpy as np
 
 import soldier
-import tailor
 
 print("Training on subset of MNIST Handwritten Digits dataset")
 
 # Load data
 print("Loading data...")
-data = loadmat("./data/test/ex4data1.mat")
-training_data = np.array(data["X"])
-training_labels = np.array(data["y"])
+data_dict = loadmat("./data/test/ex4data1.mat")
+data = np.array(data_dict["X"])
+labels = np.array(data_dict["y"])
 
 # Reformat training labels
 def format_label(label):
     new_label = np.zeros((10,))
     new_label[label[0]-1] = 1
     return new_label
-training_labels = np.array(list(map(format_label, training_labels)))
+labels = np.array(list(map(format_label, labels)))
+
+# Split up data into training & test data
+# Shuffle the data first (some hackish-y stuff here)
+data = np.array(list(zip(data, labels)))
+np.random.shuffle(data)
+split = round(data.shape[0] * 0.60)
+training = data[:split]
+test = data[split:]
+
+training_data = np.array(list(map(lambda tup: tup[0], training)))
+training_labels = np.array(list(map(lambda tup: tup[1], training)))
+
+test_data = np.array(list(map(lambda tup: tup[0], test)))
+test_labels = np.array(list(map(lambda tup: tup[1], test)))
 
 # Instantiate network
 print("Instantiating neural network...")
@@ -36,3 +49,13 @@ print("Training neural network...")
 np.seterr(over='raise')
 nn.train(training_data, training_labels)
 print(" Done")
+
+# Calculate training error
+predictions, _ = nn.predict(training_data)
+training_error = np.mean(np.double(predictions == training_labels)) * 100
+print("Training error: ", training_error)
+
+# Calculate test error
+predictions, _ = nn.predict(test_data)
+test_error = np.mean(np.double(predictions == test_labels)) * 100
+print("Test error: ", test_error)
