62a106a87c73358a1afe4a105b1263529a08bf09
Charles Dawson <charles.dwsn@gmail.com>
2015-02-09 15:25:07 -0500

    No joy with pure technical indicators. Going to run some analysis to determine future course. Moving experiment.py to param_matix_experiment.py.

--- circus/tinker.py
+++ circus/tinker.py
@@ -18,16 +18,16 @@ class Tinker(object):
         self.data_dict = scipy.io.loadmat(path_to_data)
 
     @staticmethod
-    def error(predictions, labels):
-        """Returns the % error of the neural network on the given examples"""
-        pct_error = np.mean(np.double(np.equal(predictions, labels))) * 100
+    def accuracy(predictions, labels):
+        """Returns the % accuracy of the network on the given examples"""
+        pct_accuracy = np.mean(np.double(np.equal(predictions, labels))) * 100
 
-        return pct_error
+        return pct_accuracy
 
-    def baseline(self):
+    def baseline1(self):
         """A baseline test that always "predicts" buy ([1, 0, 0])
 
-        Returns a list of [training_error, cv_error, test_error"""
+        Returns a list of [training_accuracy, cv_accuracy, test_accuracy"""
         # Load training, cv, and test
         self.data = [None] * 3
         # data[0] = training, data[1] = cv, data[2] = test
@@ -43,20 +43,77 @@ class Tinker(object):
             self.labels[i] = np.vstack([x[1] for x in dataset])
 
         training_predictions = np.vstack([[1, 0, 0] for x in self.labels[0]])
-        training_error = Tinker.error(training_predictions, self.labels[0])
+        training_accuracy = Tinker.accuracy(
+            training_predictions, self.labels[0])
         cv_predictions = np.vstack([[1, 0, 0] for x in self.labels[1]])
-        cv_error = Tinker.error(cv_predictions, self.labels[1])
+        cv_accuracy = Tinker.accuracy(cv_predictions, self.labels[1])
         test_predictions = np.vstack([[1, 0, 0] for x in self.labels[2]])
-        test_error = Tinker.error(test_predictions, self.labels[2])
+        test_accuracy = Tinker.accuracy(test_predictions, self.labels[2])
 
-        return [training_error, cv_error, test_error]
+        return [training_accuracy, cv_accuracy, test_accuracy]
+
+    def baseline1(self):
+        """A baseline test that always "predicts" ([1, 1, 1])
+
+        Returns a list of [training_accuracy, cv_accuracy, test_accuracy"""
+        # Load training, cv, and test
+        self.data = [None] * 3
+        # data[0] = training, data[1] = cv, data[2] = test
+        self.data[0] = self.data_dict["training_data"]
+        self.data[1] = self.data_dict["cv_data"]
+        self.data[2] = self.data_dict["test_data"]
+
+        # Extract labels and indicators
+        self.indicators = [None] * 3
+        self.labels = [None] * 3
+        for i, dataset in enumerate(self.data):
+            self.indicators[i] = np.vstack([x[0] for x in dataset])
+            self.labels[i] = np.vstack([x[1] for x in dataset])
+
+        training_predictions = np.vstack([[1, 1, 1] for x in self.labels[0]])
+        training_accuracy = Tinker.accuracy(
+            training_predictions, self.labels[0])
+        cv_predictions = np.vstack([[1, 1, 1] for x in self.labels[1]])
+        cv_accuracy = Tinker.accuracy(cv_predictions, self.labels[1])
+        test_predictions = np.vstack([[1, 1, 1] for x in self.labels[2]])
+        test_accuracy = Tinker.accuracy(test_predictions, self.labels[2])
+
+        return [training_accuracy, cv_accuracy, test_accuracy]
+
+    def baseline2(self):
+        """A baseline test that always "predicts" buy ([1, 0, 0])
+
+        Returns a list of [training_accuracy, cv_accuracy, test_accuracy"""
+        # Load training, cv, and test
+        self.data = [None] * 3
+        # data[0] = training, data[1] = cv, data[2] = test
+        self.data[0] = self.data_dict["training_data"]
+        self.data[1] = self.data_dict["cv_data"]
+        self.data[2] = self.data_dict["test_data"]
+
+        # Extract labels and indicators
+        self.indicators = [None] * 3
+        self.labels = [None] * 3
+        for i, dataset in enumerate(self.data):
+            self.indicators[i] = np.vstack([x[0] for x in dataset])
+            self.labels[i] = np.vstack([x[1] for x in dataset])
+
+        training_predictions = np.vstack([[1, 0, 0] for x in self.labels[0]])
+        training_accuracy = Tinker.accuracy(
+            training_predictions, self.labels[0])
+        cv_predictions = np.vstack([[1, 0, 0] for x in self.labels[1]])
+        cv_accuracy = Tinker.accuracy(cv_predictions, self.labels[1])
+        test_predictions = np.vstack([[1, 0, 0] for x in self.labels[2]])
+        test_accuracy = Tinker.accuracy(test_predictions, self.labels[2])
+
+        return [training_accuracy, cv_accuracy, test_accuracy]
 
     def with_time_series(self, num_days, hidden_layer_size, reg):
         """Trains/tests using only a time series of the past num_days
 
         Uses the given network design parameters
 
-        Returns a list of [training_error, cv_error, test_error]
+        Returns a list of [training_accuracy, cv_accuracy, test_accuracy]
         """
         # Load training, cv, and test
         self.data = [None] * 3
@@ -94,10 +151,146 @@ class Tinker(object):
         nn.train(self.indicators[0], self.labels[0])
 
         training_predictions = nn.predict(self.indicators[0])[0]
-        training_error = Tinker.error(training_predictions, self.labels[0])
+        training_accuracy = Tinker.accuracy(
+            training_predictions, self.labels[0])
         cv_predictions = nn.predict(self.indicators[1])
-        cv_error = Tinker.error(cv_predictions, self.labels[1])
+        cv_accuracy = Tinker.accuracy(cv_predictions, self.labels[1])
         test_predictions = nn.predict(self.indicators[2])
-        test_error = Tinker.error(test_predictions, self.labels[2])
+        test_accuracy = Tinker.accuracy(test_predictions, self.labels[2])
 
-        return [training_error, cv_error, test_error]
+        return [training_accuracy, cv_accuracy, test_accuracy]
+
+    def with_indicators(self, hidden_layer_size, reg):
+        """Trains/tests using only a collection of technical indicators
+
+        Uses the given network design parameters
+
+        Returns a list of [training_accuracy, cv_accuracy, test_accuracy]
+        """
+        # Load training, cv, and test
+        self.data = [None] * 3
+        # data[0] = training, data[1] = cv, data[2] = test
+        self.data[0] = self.data_dict["training_data"]
+        self.data[1] = self.data_dict["cv_data"]
+        self.data[2] = self.data_dict["test_data"]
+
+        # Extract labels and indicators
+        self.indicators = [None] * 3
+        self.labels = [None] * 3
+        for i, dataset in enumerate(self.data):
+            self.indicators[i] = np.vstack([x[0] for x in dataset])
+            self.labels[i] = np.vstack([x[1] for x in dataset])
+
+        # Map time series indicators to technical indicators
+        def calculate_indicators(ts):
+            def stochastic_k(n):
+                return 100 * (ts[n] - np.min(ts[n:n + 14])) / \
+                    (np.max(ts[n:n + 10]) - np.min(ts[n:n + 10]))
+
+            def stochastic_d(n):
+                return (stochastic_k(n) +
+                        stochastic_k(n + 1) +
+                        stochastic_k(n + 2)) / 3
+
+            def momentum(n):
+                return ts[n] - ts[n + 1]
+
+            def price_rate_of_change(n):
+                return ts[n] / ts[n + 1] * 100
+
+            def williams_pct_r(n):
+                return -100 * (np.max(ts[n:n + 5]) - ts[n]) / \
+                              (np.max(ts[n:n + 5]) - np.min(ts[n:n + 5]))
+
+            def disparity(n, moving_avg_length):
+                return 100 * ts[n] / np.mean(ts[n:n + moving_avg_length])
+
+            def price_oscillator(n):
+                return (np.mean(ts[n:n + 5]) - np.mean(ts[n:n + 10])) / \
+                    np.mean(ts[n:n + 5])
+
+            def commodity_channel_index(n):
+                # This is a bastardized version of the CCI, as we don't have
+                # . intraday Highs & Lows (yet)
+                D = np.sum(np.abs(ts[n:n + 20] - np.mean(ts[n:n + 20]))) / 20
+                return (ts[n] - np.mean(ts[n:n + 20])) / (0.015 * D)
+
+            def relative_strength_index():
+                # This is the most complicated indicator to calculate
+                # . so watch carefully
+
+                # Calculate interday changes
+                deltas = ts[:-1] - ts[1:]
+
+                # Calculate ups and downs
+                ups = np.hstack([x if x > 0 else 0 for x in deltas])
+                downs = np.hstack([abs(x) if x < 0 else 0 for x in deltas])
+
+                # Find the 10-day exponential moving average
+                # . for both the ups and the downs
+                alpha = 2.0 / (1.0 + 20.0)
+                ups_ema = 1.0
+                for price in reversed(ups):
+                    ups_ema = (price * alpha) + (ups_ema * (1 - alpha))
+                downs_ema = 1.0
+                for price in reversed(downs):
+                    downs_ema = (price * alpha) + (downs_ema * (1 - alpha))
+
+                # Compute relative strength factor
+                rs = ups_ema / downs_ema
+
+                # Return relative strength index
+                return 100 - 100 / (1 + rs)
+
+            # Slow stochastic % D is the 3-day moving average of stochastic D
+            slow_stochastic_d = (stochastic_d(0) + stochastic_d(1) +
+                                 stochastic_d(2)) / 3
+
+            # 5-day moving average
+            five_day_ma = np.mean(ts[0:6])
+
+            most_recent_price = ts[0]
+
+            return np.array([most_recent_price,
+                             five_day_ma,
+                             stochastic_k(0),
+                             stochastic_d(0),
+                             slow_stochastic_d,
+                             momentum(0),
+                             price_rate_of_change(0),
+                             williams_pct_r(0),
+                             disparity(0, 5),
+                             disparity(0, 10),
+                             price_oscillator(0),
+                             commodity_channel_index(0),
+                             relative_strength_index()])
+
+        for i, dataset in enumerate(self.indicators):
+            self.indicators[i] = np.vstack(
+                [calculate_indicators(x) for x in dataset])
+
+        # Instantiate network and train
+        input_layer_size = self.indicators[0].shape[1]
+        num_outputs = 3
+        activation_function = soldier.sigmoid
+        activation_function_gradient = soldier.sigmoid_gradient
+        output_activation_func = soldier.sigmoid
+        output_activation_func_gradient = soldier.sigmoid_gradient
+
+        nn = soldier.NeuralNetwork(input_layer_size, hidden_layer_size,
+                                   num_outputs, reg, activation_function,
+                                   activation_function_gradient,
+                                   output_activation_func,
+                                   output_activation_func_gradient)
+
+        nn.train(self.indicators[0], self.labels[0])
+
+        training_predictions = nn.predict(self.indicators[0])[0]
+        training_accuracy = Tinker.accuracy(
+            training_predictions, self.labels[0])
+        cv_predictions = nn.predict(self.indicators[1])
+        cv_accuracy = Tinker.accuracy(cv_predictions, self.labels[1])
+        test_predictions = nn.predict(self.indicators[2])
+        test_accuracy = Tinker.accuracy(test_predictions, self.labels[2])
+
+        return [training_accuracy, cv_accuracy, test_accuracy]
--- results/research_log.md
+++ results/research_log.md
@@ -1,10 +1,9 @@
-2015/02/08 - 1:20 AM
---------------------
+2015/02/08
+----------
 + Ran `experiment.py` with to test the pure time series approach with all possible combinations of:
     * Hidden Layer Size: [1, 3, 10, 30, 100, 300]
     * Regularization: [0, 1, 3, 10, 30, 100, 300, 1000]
     * Number of previous days to use in time series: [1, 3, 10, 30, 100]
-+ Determined best (least cross-validation error) combination to be:
-    * Hidden Layer Size:
-    * Regularization:
-    * Number of previous days to use in time series: 
++ Determined that, using a pure time-series based approach, cross-validation accuracies converge to 33.3333% for a number of parameter combinations.
+    * This suggests that a pure time-series based approach will bear very little fruit, as this is approximately the error that is to be expected if the neural network were to return [1, 1, 1] (buy, sell, *and* hold) for all examples. Such a prediction system indicates that the neural network has "given up", unable to detect a pattern in the data it was given, simply returns the same answer for all examples. The accuracy being dramatically higher on the training set suggests that the model suffers from high variance as well, which probably means that a pure time series approach uses too many features.
+
